e857fdd547a5a7fe1082765f40965e3a96791032
Imtiazkhilji <imtiazkhankhilji1991@gmail.com>
2017-01-22 12:31:34 +0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta name="ImtiazKhan" content="ORIC" />
+    <script src="jQuery.3.1.1/jquery-3.1.1.js"></script>
+    <script src="bootstrap.3.3.7/content/Scripts/bootstrap.js"></script>
+    <link href="bootstrap.3.3.7/content/Content/bootstrap.css" rel="stylesheet" />
+
+	<title>Vc Schedules</title>
+</head>
+<body>
+    <div class="navbar navbar-inverse navbar-fixed-top" style="background-color:rgb(51, 51, 57);height:50px;">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="~/"><img src="images/uob.png" style="height:38px;" /></a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                       <li><a href="~/">Vice Chancelor Secterate University of Balochistan</a></li>
+
+                    </ul>
+                   
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid body-content">
